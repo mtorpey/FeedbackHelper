198c5475753102e9bca593d4191b2928fb92c6d5
Michael Young <mct25@st-andrews.ac.uk>
2021-12-14 11:23:50 +0000

    Fix LoadingScreens to paint more reliably.
    It wasn't painting properly on Linux (at least for me).

--- src/main/java/view/LoadingScreens.java
+++ src/main/java/view/LoadingScreens.java
@@ -8,57 +8,83 @@ import java.awt.Toolkit;
 
 /**
  * Loading Screens Class.
+ *
+ * Used for creating splash screens, to cover periods when the program is
+ * loading. Static methods are provided which create an appropriate instance,
+ * and then delete it after a period of time; hence there are no public
+ * constructors.
  */
-public class LoadingScreens {
+public class LoadingScreens extends JWindow {
 
     // Image file paths - both images are custom made, so no licence is needed
     private static final String SPLASH_SCREEN = "/splashscreen.png";
     private static final String LOADING_SCREEN = "/loadingscreen.png";
 
     /**
-     * Show the splash screen for 10 seconds.
+     * Show a splash screen for 10 seconds.
      */
     public static void showSplashScreen() {
         showScreen(SPLASH_SCREEN, 10000);
     }
 
     /**
-     * Show the loading screen for 3 seconds.
+     * Show a loading screen for 3 seconds.
      */
     public static void showLoadingScreen() {
         showScreen(LOADING_SCREEN, 3000);
     }
 
     /**
-     * Show the a given image on a window in the middle of the screen.
+     * Show a given image on a window in the middle of the screen.
      * Code adapted from:
      * https://www.tutorialspoint.com/how-can-we-implement-a-splash-screen-using-jwindow-in-java
      */
     private static void showScreen(String imagePath, int time) {
-        // Create the loading window
-        JWindow loadingScreen = new JWindow();
-        ImageIcon imageIcon = new ImageIcon(SetupOptionsScreen.class.getResource(imagePath));
-        loadingScreen.setSize(imageIcon.getIconWidth(), imageIcon.getIconHeight());
-
-        // Centre the window
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (screenSize.width - imageIcon.getIconWidth()) / 2;
-        int y = (screenSize.height - imageIcon.getIconHeight()) / 2;
-        loadingScreen.setLocation(x, y);
-        loadingScreen.setVisible(true);
-
-        // Draw the loading screen graphics
-        Graphics graphics = loadingScreen.getGraphics();
-        loadingScreen.paint(graphics);
-        graphics.drawImage(imageIcon.getImage(), 0, 0, loadingScreen);
-
-        // Display the loading screen
+        LoadingScreens loadingScreen = new LoadingScreens(imagePath);
         try {
             Thread.sleep(time);
             loadingScreen.dispose();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    /** Image to be displayed in this window. */
+    private ImageIcon imageIcon;
+
+    /**
+     * Constructor which starts the loading screen displaying the given image.
+     *
+     * Note that this object does not delete itself, and must be deleted later
+     * e.g. by calling its dispose() method.
+     *
+     * @param imagePath Path to the image file to be displayed.
+     */
+    private LoadingScreens(String imagePath) {
+        // Create the loading window
+        imageIcon = new ImageIcon(SetupOptionsScreen.class.getResource(imagePath));
+        this.setSize(imageIcon.getIconWidth(), imageIcon.getIconHeight());
+
+        // Centre the window
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (screenSize.width - imageIcon.getIconWidth()) / 2;
+        int y = (screenSize.height - imageIcon.getIconHeight()) / 2;
+        this.setLocation(x, y);
+        this.setVisible(true);
+
+        // Draw the loading screen graphics
+        this.paint(this.getGraphics());
+    }
+
+    /**
+     * Draw the image every time this screen is repainted.
+     *
+     * @param g Should be equal to this.getGraphics().
+     */
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawImage(imageIcon.getImage(), 0, 0, this);
     }
 
 }
