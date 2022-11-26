975e587cd9adddeaba39e1b2258c4cd352b60cca
Michael Young <mct25@st-andrews.ac.uk>
2022-11-26 20:47:15 +0000

    Add flatlaf for some extra (modern) themes

--- src/main/java/view/AppView.java
+++ src/main/java/view/AppView.java
@@ -3,6 +3,9 @@ package view;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+
 import configuration.UserPreferences;
 import controller.IAppController;
 
@@ -31,10 +34,27 @@ public class AppView implements IAppView {
      */
     @Override
     public void start() {
+        installThirdPartyThemes();
         applyUserTheme();
         new HomeScreen(controller);
     }
 
+    /**
+     * Add a few extra look-and-feels for users to choose from.
+     *
+     * Currently this just adds four look-and-feels from the "flatlaf" library,
+     * which we import for this purpose.  There will also be several
+     * pre-installed look-and-feels, which will vary by OS and Java
+     * distribution.
+     */
+    private static void installThirdPartyThemes() {
+        FlatDarkLaf.installLafInfo();
+        FlatLightLaf.installLafInfo();
+        // Note: we could also install FlatDarculaLaf and FlatIntelliJLaf if we
+        // wanted, but these are almost identical to the above two themes.
+    }
+
+    /** Apply whatever theme is set in the user preferences. */
     public static void applyUserTheme() {
         String theme = UserPreferences.getTheme();
         if (theme != null) {
