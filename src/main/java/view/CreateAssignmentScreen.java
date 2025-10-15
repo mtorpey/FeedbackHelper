a49129a434ebe877213f1c4bee1e1b84a06594fc
Michael Young <mct25@st-andrews.ac.uk>
2025-10-15 11:12:40 +0100

    Rejig CreateAssignmentScreen

--- src/main/java/view/CreateAssignmentScreen.java
+++ src/main/java/view/CreateAssignmentScreen.java
@@ -104,7 +104,7 @@ public class CreateAssignmentScreen extends JFrame {
         this.controller = controller;
 
         // Setup components
-        setupTitleLabel();
+        setupFrameTitle();
         setupConfigForm();
         writeConfigOptions(configPath);
         setupConfirmationPanel();
@@ -116,44 +116,156 @@ public class CreateAssignmentScreen extends JFrame {
     }
 
     /**
-     * Setup the title label.
+     * Setup the title label at the top of the frame.
      */
-    public void setupTitleLabel() {
-        JLabel titleLabel = new JLabel("Assignment Configuration", JLabel.CENTER);
-        titleLabel.setFont(Configuration.getTitleFont());
-        add(titleLabel, BorderLayout.NORTH);
+    public void setupFrameTitle() {
+        JLabel title = new JLabel("Assignment Configuration", JLabel.CENTER);
+        title.setFont(Configuration.getTitleFont());
+        add(title, BorderLayout.NORTH);
     }
 
     /**
      * Setup the config form.
      */
     private void setupConfigForm() {
-        this.configFormPanel = new JPanel();
-        this.configFormPanel.setLayout(new GridBagLayout());
+        this.configFormPanel = new JPanel(new GridBagLayout());
         this.gridBagCounter = 0;
 
-        // Setup config components
-        setupAssignmentTitlePanel();
-        setupAssignmentDirectoryPanel();
-        setupAssignmentHeadingsPanel();
-        setupHeadingStylePanel();
-        setupHeadingUnderlinePanel();
-        setupLineMarkerPanel();
-        setupHeadingLineSpacingPanel();
-        setupStudentManifestPanel();
+        setupAssignmentTitleControls();
+        setupAssignmentDirectoryControls();
+        setupAssignmentHeadingsControls();
+        setupHeadingStyleControls();
+        setupHeadingUnderlineControls();
+        setupLineMarkerControls();
+        setupHeadingLineSpacingControls();
+        setupStudentListControls();
 
-        // Add config panel to the screen panel
         add(this.configFormPanel, BorderLayout.CENTER);
     }
 
+    /**
+     * Setup the assignment title controls.
+     */
+    private void setupAssignmentTitleControls() {
+        addLabelToConfigForm("Assignment title:");
+        assignmentTitleField = new JTextField("CS5000-P1");
+        addToConfigForm(assignmentTitleField, 2);
+    }
+
+    /**
+     * Setup the assignment directory controls.
+     */
+    private void setupAssignmentDirectoryControls() {
+        addLabelToConfigForm("Assignment directory:");
+
+        // Text field
+        String defaultDir = System.getProperty("user.home");
+        assignmentDirectoryField = new JTextField(defaultDir);
+        addToConfigForm(assignmentDirectoryField, true);
+
+        // Directory chooser
+        JButton assignmentDirectoryChooser = new JButton("Select directory");
+        assignmentDirectoryChooser.addActionListener(e ->
+            assignmentDirectoryField.setText(
+                selectPathWithDialog(
+                    assignmentDirectoryField.getText(),
+                    JFileChooser.DIRECTORIES_ONLY,
+                    "Select assignment directory...",
+                    "Submit"
+                )
+            )
+        );
+        addToConfigForm(assignmentDirectoryChooser);
+    }
+
+    /**
+     * Setup the assignment headings controls.
+     */
+    private void setupAssignmentHeadingsControls() {
+        addLabelToConfigForm("Assignment headings:");
+        assignmentHeadingsTextArea = new JTextArea(7, 30);
+        JScrollPane scrollPane = new JScrollPane(assignmentHeadingsTextArea);
+        scrollPane.setMinimumSize(new Dimension(0, 100)); // stop this collapsing
+        addToConfigForm(scrollPane, 2);
+    }
+
+    /**
+     * Setup the student list controls.
+     */
+    private void setupStudentListControls() {
+        addLabelToConfigForm("Student manifest file: ");
+
+        // Text field
+        studentListField = new JTextField();
+        addToConfigForm(studentListField, true);
+
+        // Button
+        JButton studentListFileButton = new JButton("Select file");
+        studentListFileButton.addActionListener(e ->
+            studentListField.setText(
+                selectPathWithDialog(
+                    studentListField.getText(),
+                    JFileChooser.FILES_ONLY,
+                    "Choose a student manifest file...",
+                    "Select"
+                )
+            )
+        );
+        addToConfigForm(studentListFileButton);
+    }
+
+    /**
+     * Setup the heading style controls.
+     */
+    private void setupHeadingStyleControls() {
+        addLabelToConfigForm("Heading style:");
+        headingStyleChooser = new JComboBox<String>(HEADING_STYLES.keySet().toArray(new String[0]));
+        addToConfigForm(headingStyleChooser, 2);
+    }
+
+    /**
+     * Setup the heading underline controls.
+     */
+    private void setupHeadingUnderlineControls() {
+        addLabelToConfigForm("Heading underline:");
+        underlineChooser = new JComboBox<>(UNDERLINE_STYLES.keySet().toArray(new String[0]));
+        addToConfigForm(underlineChooser, 2);
+    }
+
+    /**
+     * Setup heading line spacing controls.
+     */
+    private void setupHeadingLineSpacingControls() {
+        addLabelToConfigForm("Line spacing between sections:");
+        spacingChooser = new JComboBox<>(new Integer[] { 1, 2, 3 });
+        addToConfigForm(spacingChooser, 2);
+    }
+
+    /**
+     * Setup the line marker controls.
+     */
+    private void setupLineMarkerControls() {
+        addLabelToConfigForm("Line marker style:");
+        lineMarkerChooser = new JComboBox<>(new String[] { "-", "->", "=>", "*", "+" });
+        addToConfigForm(lineMarkerChooser, 2);
+    }
+
+    /** Add a label component to the config form, aligned right. */
+    private void addLabelToConfigForm(String text) {
+        addToConfigForm(new JLabel(text, SwingConstants.RIGHT));
+    }
+
+    /** Add a component to the config form (the main part of the window). */
     private void addToConfigForm(JComponent component) {
         addToConfigForm(component, false);
     }
 
+    /** Add a component to the config form (the main part of the window). */
     private void addToConfigForm(JComponent component, boolean fill) {
         addToConfigForm(component, fill, 1);
     }
 
+    /** Add a component to the config form (the main part of the window). */
     private void addToConfigForm(JComponent component, int width) {
         addToConfigForm(component, false, width);
     }
@@ -168,7 +280,7 @@ public class CreateAssignmentScreen extends JFrame {
      */
     private void addToConfigForm(JComponent component, boolean fill, int width) {
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.HORIZONTAL; // Expand to fill cell
+        gbc.fill = GridBagConstraints.BOTH; // Expand to fill cell
         gbc.insets = new Insets(SPACING, SPACING, SPACING, SPACING); // Padding around components
         gbc.gridx = gridBagCounter % GRID_WIDTH; // Grid is 3 cells wide
         gbc.gridy = gridBagCounter / GRID_WIDTH;
@@ -180,14 +292,24 @@ public class CreateAssignmentScreen extends JFrame {
         this.configFormPanel.add(component, gbc);
     }
 
-    private void addLabelToConfigForm(String text) {
-        addToConfigForm(new JLabel(text, SwingConstants.RIGHT));
+    /** Prompt the user to pick a file, and return the result. */
+    private String selectPathWithDialog(String startPath, int fileSelectionMode, String title, String submit) {
+        // Open file chooser
+        JFileChooser fileChooser = new JFileChooser(startPath);
+        fileChooser.setFileSelectionMode(fileSelectionMode);
+        fileChooser.setDialogTitle(title);
+
+        // Store the chosen file path
+        int returnValue = fileChooser.showDialog(this, submit);
+        String path = null;
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            path = fileChooser.getSelectedFile().getPath();
+        }
+        return path;
     }
 
     /**
-     * Setup the confirmation panel.
-     *
-     * @param configPath for prefilling values (can be null)
+     * Setup the confirmation and back buttons.
      */
     private void setupConfirmationPanel() {
         // Create panel and buttons
@@ -208,6 +330,7 @@ public class CreateAssignmentScreen extends JFrame {
         add(confirmationPanel, BorderLayout.SOUTH);
     }
 
+    /** Create new assignment using the supplied user options. */
     private void confirmClicked(ActionEvent e) {
         // Load all the user preferences
         String assignmentTitle = assignmentTitleField.getText();
@@ -219,9 +342,7 @@ public class CreateAssignmentScreen extends JFrame {
         String lineMarker = lineMarkerChooser.getItemAt(lineMarkerChooser.getSelectedIndex());
         File studentListFile = new File(studentListField.getText());
 
-        // If no student list exists before creating the feedback screen, inform the
-        // user that the
-        // software will try to guess
+        // If no student list is selected, we will try to guess
         if (studentListFile == null || !studentListFile.exists()) {
             JOptionPane.showMessageDialog(
                 this,
@@ -256,6 +377,7 @@ public class CreateAssignmentScreen extends JFrame {
         new FeedbackScreen(this.controller, assignmentFromInputs);
     }
 
+    /** Load configuration from the config file into the fields. */
     private void writeConfigOptions(String configPath) {
         //prefilled values
         if (configPath != null) {
@@ -303,128 +425,5 @@ public class CreateAssignmentScreen extends JFrame {
             .map(Map.Entry::getKey)
             .findFirst()
             .orElse(null);
-    }
-
-    /**
-     * Setup the assignment title panel.
-     */
-    private void setupAssignmentTitlePanel() {
-        addLabelToConfigForm("Assignment title:");
-        assignmentTitleField = new JTextField("CS5000-P1");
-        addToConfigForm(assignmentTitleField, 2);
-    }
-
-    /**
-     * Setup the assignment directory panel.
-     */
-    private void setupAssignmentDirectoryPanel() {
-        addLabelToConfigForm("Assignment directory:");
-
-        // Text field
-        String defaultDir = System.getProperty("user.home");
-        assignmentDirectoryField = new JTextField(defaultDir);
-        addToConfigForm(assignmentDirectoryField, true);
-
-        // Directory chooser
-        JButton assignmentDirectoryChooser = new JButton("Select directory");
-        assignmentDirectoryChooser.addActionListener(e ->
-            assignmentDirectoryField.setText(
-                selectPathWithDialog(
-                    assignmentDirectoryField.getText(),
-                    JFileChooser.DIRECTORIES_ONLY,
-                    "Select assignment directory...",
-                    "Submit"
-                )
-            )
-        );
-        addToConfigForm(assignmentDirectoryChooser);
-    }
-
-    /**
-     * Setup the assignment headings panel.
-     */
-    private void setupAssignmentHeadingsPanel() {
-        addLabelToConfigForm("Assignment headings:");
-        assignmentHeadingsTextArea = new JTextArea(7, 30);
-        JScrollPane scrollPane = new JScrollPane(assignmentHeadingsTextArea);
-        scrollPane.setMinimumSize(new Dimension(0, 100)); // stop this collapsing
-        addToConfigForm(scrollPane, 2);
-    }
-
-    /**
-     * Setup the student list panel.
-     */
-    private void setupStudentManifestPanel() {
-        addLabelToConfigForm("Student manifest file: ");
-
-        // Text field
-        studentListField = new JTextField();
-        addToConfigForm(studentListField, true);
-
-        // Button
-        JButton studentListFileButton = new JButton("Select file");
-        studentListFileButton.addActionListener(e ->
-            studentListField.setText(
-                selectPathWithDialog(
-                    studentListField.getText(),
-                    JFileChooser.FILES_ONLY,
-                    "Choose a student manifest file...",
-                    "Select"
-                )
-            )
-        );
-        addToConfigForm(studentListFileButton);
-    }
-
-    /**
-     * Setup the heading style panel.
-     */
-    private void setupHeadingStylePanel() {
-        addLabelToConfigForm("Heading style:");
-        headingStyleChooser = new JComboBox<String>(HEADING_STYLES.keySet().toArray(new String[0]));
-        addToConfigForm(headingStyleChooser, 2);
-    }
-
-    /**
-     * Setup the heading underline panel.
-     */
-    private void setupHeadingUnderlinePanel() {
-        addLabelToConfigForm("Heading underline:");
-        underlineChooser = new JComboBox<>(UNDERLINE_STYLES.keySet().toArray(new String[0]));
-        addToConfigForm(underlineChooser, 2);
-    }
-
-    /**
-     * Setup heading line spacing panel.
-     */
-    private void setupHeadingLineSpacingPanel() {
-        addLabelToConfigForm("Line spacing between sections:");
-        spacingChooser = new JComboBox<>(new Integer[] { 1, 2, 3 });
-        addToConfigForm(spacingChooser, 2);
-    }
-
-    /**
-     * Setup the line marker panel.
-     */
-    private void setupLineMarkerPanel() {
-        addLabelToConfigForm("Line marker style:");
-        lineMarkerChooser = new JComboBox<>(new String[] { "-", "->", "=>", "*", "+" });
-        addToConfigForm(lineMarkerChooser, 2);
-    }
-
-    /* Prompt the user to pick a file, and return the result. */
-    private String selectPathWithDialog(String startPath, int fileSelectionMode, String title, String submit) {
-        // Open file chooser
-        JFileChooser fileChooser = new JFileChooser(startPath);
-        fileChooser.setFileSelectionMode(fileSelectionMode);
-        fileChooser.setDialogTitle(title);
-
-        // Store the chosen file path
-        int returnValue = fileChooser.showDialog(this, submit);
-        String path = null;
-        if (returnValue == JFileChooser.APPROVE_OPTION) {
-            path = fileChooser.getSelectedFile().getPath();
-        }
-        return path;
     }
 }
