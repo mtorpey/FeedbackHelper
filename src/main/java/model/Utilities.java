368b5f4fdc0b5aa3a5f97df02e41570364a03af4
Michael Young <mct25@st-andrews.ac.uk>
2025-10-31 16:39:08 +0000

    Remove redundant Pair class

--- src/main/java/model/Utilities.java
+++ src/main/java/model/Utilities.java
@@ -52,45 +52,4 @@ public class Utilities {
         newListCopy.retainAll(oldListCopy);
         return newListCopy;
     }
-
-    /**
-     * Create a list of pairs of elements in a list.
-     *
-     * @param newList The list to create pairs from.
-     * @param <T>     The type of the data in the list.
-     * @return A list of pairs created from elements in the given list.
-     */
-    public static <T> List<Pair<T>> getPairs(List<T> newList) {
-        List<Pair<T>> pairsList = new ArrayList<Pair<T>>();
-
-        for (int i = 0; i < newList.size() - 1; i++) {
-            pairsList.add(new Pair<T>(newList.get(i), newList.get(i + 1)));
-        }
-
-        return pairsList;
-    }
-
-    /**
-     * Find pairs to remove when comparing an old list to a new list.
-     *
-     * @param oldList The old list.
-     * @param newList The new list.
-     * @param <T>     The type of the object in the lists.
-     * @return A list of pairs to remove.
-     */
-    public static <T> List<Pair<T>> getPairsToRemove(List<Pair<T>> oldList, List<Pair<T>> newList) {
-        return getRemovalsFromList(oldList, newList);
-    }
-
-    /**
-     * Find pairs to add when comparing an old list to a new list.
-     *
-     * @param oldList The old list.
-     * @param newList The new list.
-     * @param <T>     The type of the object in the lists.
-     * @return A list of pairs to add.
-     */
-    public static <T> List<Pair<T>> getPairsToAdd(List<Pair<T>> oldList, List<Pair<T>> newList) {
-        return getAdditionsToList(oldList, newList);
-    }
 }
--- src/test/java/model/UtilitiesTest.java
+++ src/test/java/model/UtilitiesTest.java
@@ -9,8 +9,6 @@ public class UtilitiesTest extends TestCase {
 
     private List<String> oldList;
     private List<String> newList;
-    private List<Pair<String>> oldPairs;
-    private List<Pair<String>> newPairs;
 
     public void setUp() throws Exception {
         oldList = new ArrayList<String>();
@@ -22,14 +20,6 @@ public class UtilitiesTest extends TestCase {
         newList.add("old-1");
         newList.add("new-1");
         newList.add("new-2");
-
-        oldPairs = new ArrayList<Pair<String>>();
-        oldPairs.add(new Pair<>("old-1", "old-2"));
-        oldPairs.add(new Pair<>("old-2", "old-3"));
-
-        newPairs = new ArrayList<Pair<String>>();
-        newPairs.add(new Pair<>("old-1", "new-1"));
-        newPairs.add(new Pair<>("new-1", "new-2"));
     }
 
     public void testGetAdditionsToList() {
@@ -50,32 +40,5 @@ public class UtilitiesTest extends TestCase {
         List<String> intersection = Utilities.getIntersection(oldList, newList);
         assertEquals(1, intersection.size());
         assertEquals("old-1", intersection.get(0));
-    }
-
-    public void testGetPairs() {
-        List<Pair<String>> pairs = Utilities.getPairs(newList);
-        assertEquals(2, pairs.size());
-        assertEquals("old-1", pairs.get(0).getFirst());
-        assertEquals("new-1", pairs.get(0).getSecond());
-        assertEquals("new-1", pairs.get(1).getFirst());
-        assertEquals("new-2", pairs.get(1).getSecond());
-    }
-
-    public void testGetPairsToRemove() {
-        List<Pair<String>> pairsToRemove = Utilities.getPairsToRemove(oldPairs, newPairs);
-        assertEquals(2, pairsToRemove.size());
-        assertEquals("old-1", pairsToRemove.get(0).getFirst());
-        assertEquals("old-2", pairsToRemove.get(0).getSecond());
-        assertEquals("old-2", pairsToRemove.get(1).getFirst());
-        assertEquals("old-3", pairsToRemove.get(1).getSecond());
-    }
-
-    public void testGetPairsToAdd() {
-        List<Pair<String>> pairsToAdd = Utilities.getPairsToAdd(oldPairs, newPairs);
-        assertEquals(2, pairsToAdd.size());
-        assertEquals("old-1", pairsToAdd.get(0).getFirst());
-        assertEquals("new-1", pairsToAdd.get(0).getSecond());
-        assertEquals("new-1", pairsToAdd.get(1).getFirst());
-        assertEquals("new-2", pairsToAdd.get(1).getSecond());
     }
 }
