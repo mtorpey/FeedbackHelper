7be67ccc7ead22484b8de58e4036138c5315f8c5
Michael Young <mct25@st-andrews.ac.uk>
2025-11-19 10:46:08 +0000

    Tweak status bar

--- src/main/java/view/StatusBar.java
+++ src/main/java/view/StatusBar.java
@@ -1,17 +1,17 @@
 package view;
 
-import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 
 public class StatusBar extends JLabel {
     public static StatusBar create() {
         StatusBar statusBar = new StatusBar();
-        statusBar.setText("hello");
+        statusBar.setText("");
         statusBar.setBorder(BorderCreator.statusBarBorder());
         return statusBar;
     }
 
     public void showMessage(String message) {
         setText(message);
+        repaint();
     }
 }
